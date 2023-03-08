8ab2dff63620fc144cdd6fccd82b7227b6c30741
ZhangPeizhou <zhangpeizhou0125@gmail.com>
2023-03-08 11:28:31 -0500

    Update Base62LtS.java

--- service/src/main/java/com/shortlink/service/server/Base62LtS.java
+++ service/src/main/java/com/shortlink/service/server/Base62LtS.java
@@ -15,7 +15,7 @@ public class Base62LtS {
     }
 
     public String encode(String url){
-        long numUrl = url.split("https://www.")[1].split(".com")[0].hashCode();
+        long numUrl = url.hashCode();
         String result = base62(numUrl);
         return  "http://localhost:7000/stl/"+result;
     }
